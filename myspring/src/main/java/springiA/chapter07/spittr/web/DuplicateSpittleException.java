ef23a175f0eccd6c5d4fcb96e2440968551d03de
chen_wei19 <chenw0571@outlook.com>
2017-09-23 12:13:42 +0800

    del

--- myspring/src/main/java/springiA/chapter07/spittr/web/DuplicateSpittleException.java
+++
@@ -1,5 +0,0 @@
-package springiA.chapter07.spittr.web;
-
-public class DuplicateSpittleException extends RuntimeException {
-
-}
--- myspring/src/main/java/springiA/chapter07/spittr/web/SpittleNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package springiA.chapter07.spittr.web;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Spittle Not Found")
-public class SpittleNotFoundException extends RuntimeException {
-
-}
