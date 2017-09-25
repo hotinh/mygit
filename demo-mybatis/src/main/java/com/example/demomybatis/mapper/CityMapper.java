620229b004b5d776daa46f4269aa7237ba6e24cf
chenw <chenw0571@outlook.com>
2017-09-25 15:49:20 +0800

    demo-mybatis

---
+++ demo-mybatis/src/main/java/com/example/demomybatis/mapper/CityMapper.java
@@ -0,0 +1,18 @@
+package com.example.demomybatis.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.demomybatis.domain.City;
+
+/**
+ * @author Eddú Meléndez
+ */
+@Mapper
+public interface CityMapper {
+
+	@Select("select * from city where state = #{state}")
+	City findByState(@Param("state") String state);
+
+}
