7285c03734885c2e8f21154190a5f12d4c1859b0
chenw <chenw0571@outlook.com>
2017-11-18 11:46:42 +0800

    del

--- ch03/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into Reader (username, password, fullname) values ('craig', 'password', 'Craig Walls');
-insert into Reader (username, password, fullname) values ('walt', 'marceline', 'Walt Disney');
-insert into Reader (username, password, fullname) values ('cc', 'cc', 'cc');
