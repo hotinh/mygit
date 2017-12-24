0d052cd2ff140a5aa8079b9f352f432db6c2f576
chenw <chenw0571@outlook.com>
2017-12-24 09:16:30 +0800

    add

---
+++ mp-wb/src/main/java/cn/cc/mpwb/MpWbApplication.java
@@ -0,0 +1,12 @@
+package cn.cc.mpwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpWbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpWbApplication.class, args);
+	}
+}
