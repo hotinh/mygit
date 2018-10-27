f4fdaf7873c86986fd42feba69c752509357a7f1
chenw <chenw0571@outlook.com>
2018-10-27 21:49:16 +0800

    add

---
+++ mystudy/Effective-Java/src/main/java/org/effectivejava/examples/chapter04/item17/Super.java
@@ -0,0 +1,13 @@
+package org.effectivejava.examples.chapter04.item17;
+
+public class Super {
+    // Broken - constructor invokes an overridable method
+
+    public Super() {
+        overrideMe();
+    }
+
+    public void overrideMe() {
+
+    }
+}
